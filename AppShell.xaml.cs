8c924d5c9a3e7e05c27b5ca62125d404176c01e7
Mateusz Świetlicki <mateusz@swietlicki.net>
2023-04-17 16:52:07 +0200

    Init with dotnet

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ms_drug_dealer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace ms_drug_dealer;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
